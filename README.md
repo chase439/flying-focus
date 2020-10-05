851a1e56018338655e56557a0452c88c595b7f79
Chase Tran <rapidror@gmail.com>
2020-10-05 03:21:11 +0000

    modularize package to work with Turbolinks

--- README.md
+++ README.md
@@ -1,12 +1,40 @@
 # Improvements to Original Repository
 
-This is a fork of [flying-focus](https://github.com/NV/flying-focus/). In the original repository, the focus disappears after 150ms. In this fork, it will remain visible when navigating using tab. Also, this fork has a package.json, separate src files for js and scss.
+This is a fork of [flying-focus](https://github.com/NV/flying-focus/). In the original repository, the focus disappears after 150ms. In this fork, it will remain visible when navigating using tab. Also, this fork:
+ - adds package.json
+ - separates src files (js and scss)
+ - modularlizes the libary to work with [turbolinks](https://github.com/turbolinks/turbolinks)
+ - jslints javascript
+ - removes unnecessary files for the standalone version.
 
+# To Use
+
+Install the package:
 ```bash
 $ npm install @chase439/flying-focus
 ```
 
-All other files that aren't necessary for the standalone version were removed.
+If you're using Turbolinks and jQuery,
+```javascript
+// Require the javascript inside your application.js
+require("@chase439/flying-focus/src/js/flying-focus.js")
+
+// Import stylesheet inside your application.scss
+@import "~@chase439/flying-focus/src/scss/flying-focus";
+
+// Activate FlyingFocus
+$( document ).on('turbolinks:load', function() {
+  FlyingFocus.start();
+});
+```
+
+If you're using native JavaScript, add the script and stylesheet files to your HTML and then activate it:
+```javascript
+// Activate FlyingFocus
+document.addEventListener("DOMContentLoaded", function() {
+  FlyingFocus.start();
+});
+```
 
 # [Focus Transition](http://n12v.com/focus-transition/)
 
--- src/js/flying-focus.js
+++ src/js/flying-focus.js
@@ -1,7 +1,10 @@
-(function() {
-  "use strict";
+/*jslint browser */
+/*global window */
 
-  document.addEventListener("DOMContentLoaded", function() {
+window.FlyingFocus = window.FlyingFocus || {};
+
+window.FlyingFocus.start = function () {
+    "use strict";
 
     var TRANSITION_DURATION = 150;
     var ringElem = null;
@@ -12,100 +15,96 @@
     var docElem = doc.documentElement;
     var body = doc.body;
 
-    docElem.addEventListener("keydown", function(event) {
-      var code = event.which;
-      // Show animation only upon Tab or Arrow keys press.
-      if (code === 9 || (code > 36 && code < 41)) {
-        keyDownTime = Date.now();
-      }
-    }, false);
-
-    docElem.addEventListener("mousedown", function(event) {
-      if(ringElem) {
-        onEnd();
-      }
-    }, false);
-
-    docElem.addEventListener("focus", function(event) {
-      var target = event.target;
-      if (target.id === "flying-focus") {
-        return;
-      }
-
-      var isFirstFocus = false;
-      if (!ringElem) {
-        isFirstFocus = true;
-        initialize();
-      }
-
-      var offset = offsetOf(target);
-      ringElem.style.left = offset.left + "px";
-      ringElem.style.top = offset.top + "px";
-      ringElem.style.width = target.offsetWidth + "px";
-      ringElem.style.height = target.offsetHeight + "px";
-
-      if(isFirstFocus){
-        return;
-      }
-
-      if (!isJustPressed()) {
-        onEnd();
-        return;
-      }
-
-      onEnd(true);
-      target.classList.add("flying-focus_target");
-      ringElem.classList.add("flying-focus_visible");
-      prevFocused = target;
-    }, true);
-
-
-    docElem.addEventListener("blur", function() {
-      onEnd(true);
-    }, true);
-
-
     function initialize() {
-      // Check if the element already exists to prevent duplicates
-      if (document.getElementById("flying-focus")) {
-        return;
-      }
+        // if the element exists, don't create it; prevents duplicate
+        if (document.getElementById("flying-focus")) {
+            return;
+        }
 
-      // Use uniq element name to decrease the chances of a conflict with website styles
-      ringElem = doc.createElement("flying-focus");
-      ringElem.id = "flying-focus";
-      ringElem.style.transitionDuration = ringElem.style.WebkitTransitionDuration = TRANSITION_DURATION / 1000 + "s";
-      body.appendChild(ringElem);
+        ringElem = doc.createElement("flying-focus");
+        ringElem.id = "flying-focus";
+        ringElem.style.WebkitTransitionDuration = TRANSITION_DURATION / 1000 + "s";
+        ringElem.style.transitionDuration = ringElem.style.WebkitTransitionDuration;
+        body.appendChild(ringElem);
     }
 
     function onEnd(keepRing) {
-      if (keepRing) {
-        return;
-      }
-      ringElem.classList.remove("flying-focus_visible");
-      if(prevFocused){
-        prevFocused.classList.remove("flying-focus_target");
-        prevFocused = null;
-      }
+        if (keepRing) {
+            return;
+        }
+        ringElem.classList.remove("flying-focus_visible");
+        if(prevFocused){
+            prevFocused.classList.remove("flying-focus_target");
+            prevFocused = null;
+        }
     }
 
     function isJustPressed() {
-      return Date.now() - keyDownTime < 42;
+        return Date.now() - keyDownTime < 42;
     }
 
     function offsetOf(elem) {
-      var rect = elem.getBoundingClientRect();
-      var clientLeft = docElem.clientLeft || body.clientLeft;
-      var clientTop = docElem.clientTop || body.clientTop;
-      var scrollLeft = win.pageXOffset || docElem.scrollLeft || body.scrollLeft;
-      var scrollTop = win.pageYOffset || docElem.scrollTop || body.scrollTop;
-      var left = rect.left + scrollLeft - clientLeft;
-      var top = rect.top + scrollTop - clientTop;
-      return {
-        top: top || 0,
-        left: left || 0
-      };
+        var rect = elem.getBoundingClientRect();
+        var clientLeft = docElem.clientLeft || body.clientLeft;
+        var clientTop = docElem.clientTop || body.clientTop;
+        var scrollLeft = win.pageXOffset || docElem.scrollLeft || body.scrollLeft;
+        var scrollTop = win.pageYOffset || docElem.scrollTop || body.scrollTop;
+        var left = rect.left + scrollLeft - clientLeft;
+        var top = rect.top + scrollTop - clientTop;
+        return {
+            top: top || 0,
+            left: left || 0
+        };
     }
 
-  });
-})();
+    docElem.addEventListener("keydown", function(event) {
+        var code = event.which;
+        // Show animation only upon Tab or Arrow keys press.
+        if (code === 9 || (code > 36 && code < 41)) {
+            keyDownTime = Date.now();
+        }
+    }, false);
+
+    docElem.addEventListener("mousedown", function(event) {
+        if(ringElem) {
+            onEnd();
+        }
+    }, false);
+
+    docElem.addEventListener("focus", function(event) {
+        var target = event.target;
+        if (target.id === "flying-focus") {
+            return;
+        }
+
+        var isFirstFocus = false;
+        if (!ringElem) {
+            isFirstFocus = true;
+            initialize();
+        }
+
+        var offset = offsetOf(target);
+        ringElem.style.left = offset.left + "px";
+        ringElem.style.top = offset.top + "px";
+        ringElem.style.width = target.offsetWidth + "px";
+        ringElem.style.height = target.offsetHeight + "px";
+
+        if(isFirstFocus){
+            return;
+        }
+
+        if (!isJustPressed()) {
+            onEnd();
+            return;
+        }
+
+        onEnd(true);
+        target.classList.add("flying-focus_target");
+        ringElem.classList.add("flying-focus_visible");
+        prevFocused = target;
+    }, true);
+
+    docElem.addEventListener("blur", function() {
+        onEnd(true);
+    }, true);
+};
